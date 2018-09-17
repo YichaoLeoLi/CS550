a462cd6c58f1c431c0b8c7662088f972ef4302d9
Yichao Li <liyichaoliyichao@126.com>
2018-09-16 21:00:00 -0400

    Telling you the score based on a number enetered in command line

---
+++ compoundinterest.py
@@ -0,0 +1,8 @@
+import sys
+
+t = float(sys.argv[1])
+P = float(sys.argv[2])
+r = float(sys.argv[3])
+
+a = P*2.71828**(r*t)
+print(a)
---
+++ rainy.py
@@ -0,0 +1,4 @@
+x = input("what is the weather like today? ")
+if x == "rainy":
+	print("You better bring your umbrella today!")
+elif x == 
