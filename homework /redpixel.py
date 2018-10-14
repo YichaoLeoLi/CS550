04d8214217e799ffa96c04e5b0d8f48977f633b2
YichaoLeoLi <43074923+YichaoLeoLi@users.noreply.github.com>
2018-10-14 01:05:51 -0400

    Update redpixel.py

--- homework /redpixel.py
+++ homework /redpixel.py
@@ -1,12 +1,16 @@
+#Leo Li
+#10/13/18
+#description: this program creates a image full of red pixel by using the image function in the python library.
+
 from PIL import Image
 
-imgx = 512
-imgy = 512
+imgx = 512#the width of the image
+imgy = 512#the height of the image
 
-image = Image.new("RGB",(imgx,imgy))
+image = Image.new("RGB",(imgx,imgy))#create a new image that has a size of imgx, imgy, and uses color mode RGB
 
 for i in range (512):
 	for j in range (512):
-		image.putpixel((i,j),(255,0,0))
+		image.putpixel((i,j),(255,0,0))#for everypixel in the image, fill in color red.
 
-image.save("image.png", "PNG")
+image.save("image.png", "PNG")#save the image
