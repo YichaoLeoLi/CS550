6f1af74c1edc877f72689dc97e00840c6900e79c
Yichao Li <liyichaoliyichao@126.com>
2018-10-26 11:09:49 -0400

    created the bank account program

---
+++ Classes!/bank.py
@@ -0,0 +1,50 @@
+
+
+
+
+class account:
+	def __init__(self):
+		self.balance = 0
+		self.accountnumber = 1234567890123456
+		self.pin 
+		self.open = False
+	def deposit(self, a):
+		if a <=20000 and a>0:
+			self.balance+=a
+		else:
+			error = "\nSorry, you cannot make a deposit that's more than $20000. Please re-enter the amount of money you would like to deposit\n>>"
+			return error
+			deposit(float(input("\nPlease enter the amount of money you want to deposit\n>>")))
+	def withdrawl(b):
+		if b <= 20000 and b > 0:
+			self.balance-=b
+		else:
+			print("\nSorry, you cannot withdraw more than $20000. Please re-enter the amount of money you want to withdraw")
+			withdrawl(float(input("\nPlease enter the amount of money you would like to withdraw\n>>")))
+
+
+def create():
+	x = int(input("\nDo you want to open an account?\n1)Yes\n2)No\n>>"))
+	if x == 1:
+		self.open != self.open
+		print("\nYour account number is " + self.accountnumber)
+		y = int(input("\nWould you like to set a pin number?\n1)Yes\n2)No\n>>"))
+		if y == 1:
+			self.pin = int.input("\nPlease enter your pin\n>>")
+		if y == 2:
+			z = int(input("\nWould you like to do something else?\n1)Yes\n2)No\n>>"))
+			if z == 1:
+				choice = int(input("\nWhat would you like to do?"))
+
+account.deposit(float(input("\nPlease enter the amount of money you want to deposit\n>>")))
+account.withdrawl(float(input("\nPlease enter the amount of money you would like to withdraw\n>>")))
+
+
+
+
+
+
+
+
+
+
---
+++ Classes!/classs.py
@@ -0,0 +1,67 @@
+#properties: hunger/thirst level, happiness, anger, energy, name, age, size
+#methods(function)(method is a part of a class while function is not): run, bark,eat, sleep, play, bite 
+
+import random
+
+class Leo:
+
+	#constructor
+	#scale out of 100
+	def __init__(self, homework):#homework here is a variable that holds a value
+		self.fullness = 5
+		self.energy = 5
+		self.happiness = 5
+		self.homework = homework#self.homework is the class property
+
+
+		#methods
+	def play(self):
+		if self.energy>0 and self.fullness>0:
+			self.happiness+=1
+			self.fullness-=1
+			self.energy-=1
+			status = "I played"
+			return status
+		else:
+			status = "You played too much and you are dead :("
+			return status
+			quit()
+		
+
+	def playsquash(self):
+		if self.energy>0 and self.fullness>0:
+			self.happiness+=random.randint(-5,5)
+			self.fullness-=30
+			self.energy-=30
+			status = "Played sqaush"
+			# self.stats()
+			return status
+		else:
+			status = "You died while playing squash because you are out of energy"
+			return status
+			quit()
+
+	def stats(self):
+		info + "\nenergy:" + str(self.energy)
+		info += "\nfullness:" + str(self.fullness)
+		info += "\nhappiness:" + str(self.happiness)
+		info += "\nhappiness:" + str(self.homework)
+
+		return info
+
+
+
+#print(leo1.name)#accessing the property outside fo the calss
+
+while True:
+	print(leo1.stats())
+	choice = input("what would you like to do with your dog")
+	if choice == "play":
+		print(leo1.play())
+	else:
+		print("You can't do that")
+
+
+
+
+
