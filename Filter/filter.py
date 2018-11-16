e72e88cd26c92e4ef4c36cc5d17f329632e19f04
Yichao Li <liyichaoliyichao@126.com>
2018-11-16 12:01:17 -0500

    Classwork Fri

--- Filter/filter.py
+++ Filter/filter.py
@@ -1,3 +1,8 @@
+#Leo Li
+#11/16/2018
+#Description: This is the sobel filter. It traces out the edge of an image and blacks out everything else
+#source:http://homepages.inf.ed.ac.uk/rbf/HIPR2/sobel.htm
+
 
 
 from PIL import Image
@@ -5,15 +10,14 @@ import sys
 import math
 
 def main():
-	global img
-	img = Image.open(sys.argv[1])
+	global img,j,w
+	img = Image.open(sys.argv[1])#using the commandline argument to open the file
 	w,h = img.size
 	newimg = Image.new("RGB", (w,h), "white")
-	for j in range(1, w-1):
+	for j in range(1, w-1):#for every single pixel in the picture(except the corners)
 		for i in range(1, h-1):
-			print(i,j)
 			Gx = 0
-			Gy = 0
+			Gy = 0#add values of gradients to each pixel around it according to the kernel
 			takepixels(j-1,i-1)
 			Gx+=-x
 			Gy+=x
@@ -39,11 +43,13 @@ def main():
 			Gx+=x
 			Gy+=-x
 
-			l = math.sqrt(Gx**2+Gy**2)
-			l = int(l/w*255)
+			l = math.sqrt(Gx**2+Gy**2)#calculate the length
+			l = int(l/w*255)#scale the image
 			newimg.putpixel((j,i),(l,l,l))
+			percentage()
 	
 	newimg.show()
+	newimg.save("Image1.png", "PNG")
 
 def takepixels(a, b):
 	global img,x
@@ -53,7 +59,11 @@ def takepixels(a, b):
 	b = pixel[2]
 	x = r+g+b
 
-
+#it shows the percentage of the program since sometimes it takes a long time for the program to process
+def percentage():
+	p = j/w*100
+	print(str(p)+"%")
+	
 
 
 main()
